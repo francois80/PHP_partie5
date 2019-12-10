7cde4e5a5e9e55f1ae8d3811a8703e7a291c9b75
francois80 <glavieux.francois@orange.fr>
2019-12-10 13:40:13 +0100

    Ajout exercice 7 partie5

---
+++ exo7/index.php
@@ -0,0 +1,14 @@
+<?php
+  $exercice ='Exercice 7';
+  include '../header.php';
+  $tabRegionHDF = ['02'=>'Aisne','59'=>'Nord','60'=>'Oise','62'=>'Pas de Calais','80'=>'Somme'];
+  $tabRegionHDF['51'] = 'Reims';
+?>
+<h1>Avec le tableau de l'exercice 5, ajouter la ligne correspondant au département de la ville de Reims.</h1>
+<?php foreach ($tabRegionHDF as $key => $value) { ?>
+  <p><?= $key ?> <?= $value ?> </p>
+<?php
+}
+?>
+  </body>
+</html>
