0641a928a6630ae4227ec2e30229ba98437e2383
francois80 <glavieux.francois@orange.fr>
2019-12-10 13:41:44 +0100

    Ajout du header partie5

---
+++ header.php
@@ -0,0 +1,24 @@
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title><?= $exercice ?> PHP partie4</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active"><a class="nav-link" href="http://www.phppartie5exo1.com/">Exercice 1</a></li>
+				<li class="nav-item active"><span id="pipe-bar" class="nav-link">|</span></li>
+				<li class="nav-item active"><a class="nav-link" href="http://www.phppartie5exo2.com/">Exercice 2</a></li>
+				<li class="nav-item active"><a class="nav-link" href="http://www.phppartie5exo3.com/">Exercice 3</a></li>
+				<li class="nav-item active"><a class="nav-link" href="http://www.phppartie5exo4.com/">Exercice 4</a></li>
+				<li class="nav-item active"><a class="nav-link" href="http://www.phppartie5exo5.com/">Exercice 5</a></li>
+				<li class="nav-item active"><a class="nav-link" href="http://www.phppartie5exo6.com/">Exercice 6</a></li>
+        <li class="nav-item active"><a class="nav-link" href="http://www.phppartie5exo7.com/">Exercice 7</a></li>
+        <li class="nav-item active"><a class="nav-link" href="http://www.phppartie5exo8.com/">Exercice 8</a></li>
+        <li class="nav-item active"><a class="nav-link" href="http://www.phppartie5exo9.com/">Exercice 9</a></li>
+        <li class="nav-item active"><a class="nav-link" href="http://www.phppartie5exo10.com/">Exercice 10</a></li>
+			</ul>
+    </div>
+  </nav>
