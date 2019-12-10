39b82a474fe5490310de2d674618b1d11a757fb5
francois80 <glavieux.francois@orange.fr>
2019-12-10 13:39:01 +0100

    Ajout exercice 3 partie5

---
+++ exo3/index.php
@@ -0,0 +1,12 @@
+<?php
+  $exercice ='Exercice 3';
+  include '../header.php';
+  $tabMonth = ['janvier','février','mars','avril','mai','juin','juiller','aout','septempbre',
+              'novembre','décembre'];
+  $mois = $tabMonth[5];
+?>
+<h1>Avec le tableau de l'exercice 1, afficher la valeur de l'index 5.
+</h1>
+  <p><?= $mois ?></p>
+  </body>
+</html>
