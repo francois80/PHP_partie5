f39940606e6c6f38e62e039383b85833572ab0b9
francois80 <glavieux.francois@orange.fr>
2019-12-20 14:36:22 +0100

    Correction

--- exo10/index.php
+++ exo10/index.php
@@ -4,12 +4,12 @@
   $tabRegionHDF = ['02'=>'Aisne','59'=>'Nord','60'=>'Oise','62'=>'Pas de Calais','80'=>'Somme'];
   $tabRegionHDF['51'] = 'Reims';
 ?>
-<h3>Avec le tableau de l'exercice 5, afficher toutes les valeurs de ce tableau ainsi que les clés associés.<br>
-Cela pourra être, par exemple, de la forme : "Le département" + nom du département + "a le numéro" + numéro du département</h3>
-<?php foreach ($tabRegionHDF as $key => $value) { ?>
-  <p>Le département <?= $value ?> à le numéro <?= $key ?>.</p>
-<?php
-}
-?>
-  </body>
+  <h3>Avec le tableau de l'exercice 5, afficher toutes les valeurs de ce tableau ainsi que les clés associés.<br>
+  Cela pourra être, par exemple, de la forme : "Le département" + nom du département + "a le numéro" + numéro du département</h3>
+  <?php foreach ($tabRegionHDF as $key => $value) { ?>
+    <p>Le département <?= $value ?> à le numéro <?= $key ?>.</p>
+  <?php
+  }
+  ?>
+</body>
 </html>
--- exo7/index.php
+++ exo7/index.php
@@ -2,7 +2,7 @@
   $exercice ='Exercice 7';
   include '../header.php';
   $tabRegionHDF = ['02'=>'Aisne','59'=>'Nord','60'=>'Oise','62'=>'Pas de Calais','80'=>'Somme'];
-  $tabRegionHDF['51'] = 'Reims';
+  $tabRegionHDF['51'] = 'Marne';
 ?>
 <h1>Avec le tableau de l'exercice 5, ajouter la ligne correspondant au département de la ville de Reims.</h1>
 <?php foreach ($tabRegionHDF as $key => $value) { ?>
